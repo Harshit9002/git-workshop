32f35078e23c0fffc17c858b81a7cb2780ede9a1
Harshit <harshit@leewayhertz.com>
2022-10-19 16:26:35 +0530

    upd

--- m.js
+++ m.js
@@ -15,6 +15,13 @@ const e = a * b;
 const number = parseInt(prompt('Enter the number of terms: '));
 let n1 = 0, n2 = 1, nextTerm;
 
-kknwjnfjjbjbfj  wf fwkn
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
+  
 console.log('multiply of ' + a + ' and ' + b + ' is ' + c);
-jnkjbwb kw kncs

824ce9634927b052b86abbd1beacc8b77a011395
Harshit <harshit@leewayhertz.com>
2022-10-19 15:36:13 +0530

    development code change

--- m.js
+++ m.js
@@ -6,16 +6,17 @@ const c = a * b;
 const d = 3;
 
 
+
 const e = a * b;
 
 
-
   //// Fibonacci 
-  const number = parseInt(prompt('Enter the number of terms: '));
-  let n1 = 0, n2 = 1, nextTerm;
-  
-  console.log('Fibonacci Series:');
-  
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+
+
+console.log('multiply of ' + a + ' and ' + b + ' is ' + c);
+
 
   
-console.log('multiply of ' + a + ' and ' + b + ' is ' + c);

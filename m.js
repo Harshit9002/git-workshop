7fecffac453642a780ff286384886c16f69f3989
Harshit <harshit@leewayhertz.com>
2022-10-19 16:47:56 +0530

    updated

--- m.js
+++ m.js
@@ -9,12 +9,22 @@ const d = 3;
 const e = a * b;
 
 
-
 // JavaScript program to find the area of a triangle
 
 const side1 = parseInt(prompt('Enter side1: '));
 const side2 = parseInt(prompt('Enter side2: '));
 const side3 = parseInt(prompt('Enter side3: '));
 
+const s = (side1 + side2 + side3) / 2;
 
+const areaValue = Math.sqrt(
+  s * (s - side1) * (s - side2) * (s - side3)
+);
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
+
+
+  
 console.log('multiply of ' + a + ' and ' + b + ' is ' + c);

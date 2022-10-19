7be88082ab940df3f4bc9b6d2c83fd257e59f6b7
Harshit <harshit@leewayhertz.com>
2022-10-19 16:37:13 +0530

    updated

--- m.js
+++ m.js
@@ -10,10 +10,21 @@ const e = a * b;
 
 
 
-// fibonacii
+// JavaScript program to find the area of a triangle
 
-const number = parseInt(prompt('Enter the number of terms: '));
-let n1 = 0, n2 = 1, nextTerm;
+const side1 = parseInt(prompt('Enter side1: '));
+const side2 = parseInt(prompt('Enter side2: '));
+const side3 = parseInt(prompt('Enter side3: '));
 
+const s = (side1 + side2 + side3) / 2;
+
+
+const areaValue = Math.sqrt(
+  s * (s - side1) * (s - side2) * (s - side3)
+);
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
 
 console.log('multiply of ' + a + ' and ' + b + ' is ' + c);

cfb41658cc33410df2bbdfb713be43452a97bcbd
Harshit <harshit@leewayhertz.com>
2022-10-19 15:15:56 +0530

    updated

--- m.js
+++ m.js
@@ -16,11 +16,6 @@ const e = a * b;
   
   console.log('Fibonacci Series:');
   
-  for (let i = 1; i <= number; i++) {
-      console.log(n1);
-      nextTerm = n1 + n2;
-      n1 = n2;
-      n2 = nextTerm;
-  }
+
   
 console.log('multiply of ' + a + ' and ' + b + ' is ' + c);

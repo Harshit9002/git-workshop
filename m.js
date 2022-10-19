ceb79e2f08aae4b2af233b1316106d827d7db222
Harshit <harshit@leewayhertz.com>
2022-10-19 12:40:12 +0530

    fibonacci added

--- m.js
+++ m.js
@@ -9,3 +9,18 @@ const d = 3;
 const e = a * b;
 
 console.log('multiply of ' + a + ' and ' + b + ' is ' + c);
+
+
+
+  //// Fibonacci 
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
